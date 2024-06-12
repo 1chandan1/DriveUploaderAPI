9494f8cc837f30f33aa44c0240d43b22199dab7e
ChandanHans <chandanhans2003@gmail.com>
2024-06-12 13:29:07 +0530

    sign-date added

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import asyncio
 import base64
 import os
+import re
+import io
 import json
+import fitz  # PyMuPDF
+from PIL import Image
 from openai import OpenAI
+from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 from google.oauth2 import service_account
 from fastapi.responses import JSONResponse
 from concurrent.futures import ThreadPoolExecutor
 from google.auth.transport.requests import Request
 from fastapi import FastAPI, HTTPException, status, Header, File, UploadFile
-from PIL import Image
-import fitz  # PyMuPDF
-import io
 
 # Load environment variables from .env file
 load_dotenv(dotenv_path=".env")
@@ -28,33 +30,6 @@ scopes = ["https://www.googleapis.com/auth/drive"]
 credentials = service_account.Credentials.from_service_account_info(
     creds_dict, scopes=scopes
 )
-
-# Initialize FastAPI app
-app = FastAPI()
-
-# Initialize ThreadPoolExecutor
-executor = ThreadPoolExecutor(max_workers=4)
-
-@app.head("/")
-@app.get("/")
-async def root():
-    """Root endpoint, publicly accessible."""
-    return {"message": "API is live!"}
-
-
-@app.post("/access_token")
-async def access_token(api_key: str = Header(...)):
-    """Secured endpoint to get the access token."""
-    if api_key != api_key_secret:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or missing API Key",
-        )
-    print(credentials.expired)
-    if credentials.expired or not credentials.token:
-        credentials.refresh(Request())
-    return {"access_token": credentials.token}
-
 
 def get_image_result(base64_image):
     prompt = """
@@ -102,8 +77,51 @@ def get_dates_with_gpt(file_data: bytes, resolution: int = 200) -> str:
     except Exception as e:
         raise
 
+def extract_signed_date_from_pdf(file):
+    reader = PdfReader(file)
+    text = ""
+    # Extract text from each page
+    for page_num in range(len(reader.pages)):
+        page = reader.pages[page_num]
+        text += page.extract_text()
 
-@app.post("/pdf_dates")
+    # Regular expression to find dates in the format dd/mm/yyyy and dd / mm / yyyy
+    date_pattern = re.compile(r'\b\d{2}\s*/\s*\d{2}\s*/\s*\d{4}\b')
+    dates : list[str] = date_pattern.findall(text)
+    
+    # Assuming the signing date is the last date in the document
+    if dates:
+        signing_date = dates[-1].replace(" ","")
+        return signing_date
+    else:
+        return None
+    
+# Initialize FastAPI app
+app = FastAPI()
+executor = ThreadPoolExecutor(max_workers=4)
+
+@app.head("/")
+@app.get("/")
+async def root():
+    """Root endpoint, publicly accessible."""
+    return "API is live!"
+
+@app.post("/access-token")
+async def access_token(api_key: str = Header(...)):
+    """Secured endpoint to get the access token."""
+    if api_key != api_key_secret:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing API Key",
+        )
+    print(credentials.expired)
+    if credentials.expired or not credentials.token:
+        credentials.refresh(Request())
+    return {"access_token": credentials.token}
+
+
+    
+@app.post("/dob-dod")
 async def ocr(file: UploadFile = File(...)):
     """Endpoint to perform OCR on a PDF file and extract DOB and DOD."""
     file_data = await file.read()
@@ -114,8 +132,13 @@ async def ocr(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@app.post("/sign-date")
+async def extract_date(file: UploadFile = File(...)):
+    content = await file.read()
+    file_like_object = io.BytesIO(content)
+    signing_date = extract_signed_date_from_pdf(file_like_object)
+    return {"sign_date": signing_date}
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
